47b703bf61a57fae1fb5009f842f32b567638c24
synthe102 <leonard@suslian.engineer>
2025-03-09 12:40:47 +0000

    feat: add devenv

---
+++ devenv.nix
@@ -0,0 +1,20 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+    clusterctl
+    talosctl
+    talhelper
+    just
+    sops
+    age
+    k9s
+    cilium-cli
+  ];
+}
